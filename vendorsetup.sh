58a0df0194297138d99a578c61a72711d35fe830
Charles078728 <168327347+Charles078728@users.noreply.github.com>
2025-12-24 18:16:15 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,31 +1,28 @@
 #
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2023 The OrangeFox Recovery Project
-#	
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
+# Copyright (C) 2023 The OrangeFox Recovery Project
 #
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-# 	
-# 	Please maintain this if you use this script or any part of it
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-export FDEVICE="BF7"
+FDEVICE="BF7"
+#set -o xtrace
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-   if [ -n "$chkdev" ]; then
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -35,6 +32,45 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+    export FOX_VERSION="R11.1"
+    export FOX_BUILD_TYPE=Unofficial
+    # export FOX_ENABLE_LAB=1
+    
+    export TARGET_DEVICE_ALT="Tecno-BF7"
+      export OF_MAINTAINER="aminegames125"
+
+    export OF_AB_DEVICE=1
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+
+    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+
+    export OF_ADVANCED_SECURITY=1
+
+    # Temp
+    export OF_NO_SPLASH_CHANGE=1
+
+    export FOX_REMOVE_AAPT=1
+    export FOX_DISABLE_APP_MANAGER=1
+    export FOX_DELETE_AROMAFM=1
+
+    export OF_STATUS_INDENT_LEFT=64
+    export OF_STATUS_INDENT_RIGHT=64
+
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+
+
+    # export FOX_BOARD_AVB_ENABLE=1
+    # export AVBTOOL=external/avb
+fi
+#if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	 export TW_DEFAULT_LANGUAGE="en"
     	export LC_ALL="C"
     	export FOX_AB_DEVICE=1
